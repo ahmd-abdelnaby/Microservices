8078940cba05bc12d705ad731f8ca2949bcf585f
mohamed.abdelaleem@excelsystems-eg.com <mohamed.abdelaleem@excelsystems-eg.com>
2022-11-29 17:22:30 +0200

    add extension to health check

---
+++ Helathcheck/Extensions/HealthCheckExtension.cs
@@ -0,0 +1,41 @@
+﻿using HealthChecks.System;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Security.Claims;
+
+namespace Helathcheck.Extensions
+{
+    public static class HealthCheckExtension
+    {
+        public static IServiceCollection AddHealthCheck(this IServiceCollection services, WebApplicationBuilder webApplicationBuilder)
+        {
+            services.AddHealthChecks()
+                .AddDiskStorageHealthCheck(delegate (DiskStorageOptions diskStorageOptions)
+                {
+                    diskStorageOptions.AddDrive(@"C:\", 5000);
+                }, name: "My Drive", HealthStatus.Unhealthy)
+                .AddSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+            services.AddHealthChecksUI().AddInMemoryStorage();
+            return services;
+        }
+        public static WebApplication UseHealthCheck(this WebApplication app)
+        {
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            }).UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/hc-ui";
+                options.AddCustomStylesheet("./Customization/custom.css");
+            });
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            return app;
+        }
+    }
+}
--- Helathcheck/Program.cs
+++ Helathcheck/Program.cs
@@ -1,7 +1,6 @@
 using HealthChecks.System;
 using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Helathcheck.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHealthChecks()
-    .AddDiskStorageHealthCheck(delegate (DiskStorageOptions diskStorageOptions)
-    {
-        diskStorageOptions.AddDrive(@"C:\", 5000);
-    }, name: "My Drive", HealthStatus.Unhealthy)
-    .AddSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+builder.Services.AddHealthCheck(builder);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,22 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHealthChecks("/hc", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-}).UseHealthChecksUI(options =>
-{
-    options.UIPath = "/hc-ui";
-    options.AddCustomStylesheet("./Customization/custom.css");
-});
-
+app.UseHealthCheck();
 app.UseRouting();
-app.UseHealthChecks("/hc", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
