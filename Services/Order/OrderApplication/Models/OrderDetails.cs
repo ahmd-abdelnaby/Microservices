39194991f7b111721138bb3df376c0a906a7fb60
ali.khalaf <ali.khalaf@ALI-KHALAF>
2022-12-06 18:40:03 +0200

    add order and order details to databse

---
+++ Services/Order/OrderApplication/Models/OrderDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace OrderApplication.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; } 
+        public int OderId { get; set; }    
+        public int ProductId { get; set; }    
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+
+        public virtual Order Order { get; set; }
+
+
+
+    }
+}
