a4d1ae3adb5095cd34948f47baa2107c889177ff
ahmedabdelnaby1993 <ahmedabdelnaby1993@gmail.com>
2022-12-06 17:03:39 +0200

    configure mass trasnsit

---
+++ MassTransitConsumer/Saga/OrderStateMachine.cs
@@ -0,0 +1,52 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassTransitConsumer.Saga
+{
+    public class OrderStateMachine : MassTransitStateMachine<OrderState>
+    {
+        public OrderStateMachine()
+        {
+            Event(() => SubmitOrder, x => x.CorrelateById(context => context.Message.OrderId));
+
+            Initially(
+                When(SubmitOrder)
+                    .Then(x => x.Saga.OrderDate = x.Message.OrderDate)
+                    .TransitionTo(Submitted),
+                When(OrderAccepted)
+                    .TransitionTo(Accepted));
+
+            During(Submitted,
+                When(OrderAccepted)
+                    .TransitionTo(Accepted));
+
+            During(Accepted,
+                When(SubmitOrder)
+                    .Then(x => x.Saga.OrderDate = x.Message.OrderDate));
+        }
+
+        public Event<SubmitOrder> SubmitOrder { get; private set; }
+        public Event<OrderAccepted> OrderAccepted { get; private set; }
+        public State Submitted { get; private set; }
+        public State Accepted { get; private set; }
+    }
+
+    public interface SubmitOrder
+    {
+        Guid OrderId { get; }
+
+        DateTime OrderDate { get; }
+    }
+
+    public class OrderState : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+    }
+}
