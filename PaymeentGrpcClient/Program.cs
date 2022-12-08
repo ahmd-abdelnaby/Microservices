b14d2ca59545f2dced2d885cff4492629f59700b
Ahmed Badr <ahmed.badr@hotmail.com>
2022-12-08 12:35:41 +0200

    Add gRPC services

---
+++ PaymeentGrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+Console.ReadLine();
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+var client = new PaymentGrpcService.PaymentServices.PaymentServicesClient(channel);
+
+var response = client.PaymentDetailsByOrderId(new PaymentGrpcService.OrderIdRequest() { Id = 2});
+
+while (response.ResponseStream.MoveNext().Result)
+{
+    Console.WriteLine(response.ResponseStream.Current.Payment.ToString());
+}
+
+Console.WriteLine("Finished");
+Console.ReadLine();
