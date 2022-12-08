074f7f7c891b7f53c7c2da49bc7101e7e4f96023
ahmedabdelnaby1993 <ahmedabdelnaby1993@gmail.com>
2022-12-08 13:13:38 +0200

    change databse to 248 server

--- Product.Infrastrucure/ProductContextFactory.cs
+++ Product.Infrastrucure/ProductContextFactory.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace Product.Infrastrucure
 {
-    public class BloggingContextFactory : IDesignTimeDbContextFactory<ProductContext>
+    public class ProductContextFactory : IDesignTimeDbContextFactory<ProductContext>
     {
         public ProductContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ProductContext>();
-            optionsBuilder.UseSqlServer("Data Source=localhost\\MSSQLSERVER02;Initial Catalog=Product;Integrated Security=True;Trust Server Certificate=true");
+            optionsBuilder.UseSqlServer("Data Source=197.168.1.248;Initial Catalog=MirageMSProduct;User Id=sa;Password=ExcelSystems@2017;TrustServerCertificate=True;");
 
             return new ProductContext(optionsBuilder.Options);
         }
